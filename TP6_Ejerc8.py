dedd2331f5a8328c3d747966940b897fc34dd098
Abel Zambrana <70001008+abelzambrana@users.noreply.github.com>
2023-07-08 16:47:46 -0300

    Add files via upload
    Ejercicio sencillo en python

---
+++ TP6_Ejerc8.py
@@ -0,0 +1,12 @@
+def main ():
+    dividendo=int(input("Ingrese el dividendo de la operación: "))
+    divisor=int(input("Ingrese el divisor de la operación: "))
+    if divisor==0:
+        print("No se puede dividir por 0!")
+    else:
+        resultado=dividendo/divisor
+        print("El resultado de la operacion dividir es",resultado)
+    print("Saludos")
+    
+if __name__=="__main__":
+     main()
