4cfdfb86e041c75f483608a0221eadcda64926a4
Abel Zambrana <abel.led94@gmail.com>
2023-07-24 23:35:46 -0300

    Update TP6_Ejerc8.py

--- TP6_Ejerc8.py
+++ TP6_Ejerc8.py
@@ -2,12 +2,9 @@ def main ():
     dividendo=int(input("Ingrese el dividendo de la operación: "))
     divisor=int(input("Ingrese el divisor de la operación: "))
 
-#apartir de aqui comienza el if
-
     if divisor==0:
         print("No se puede dividir por 0!")
 
-#apartir de aqui comienza el else
     else:
         resultado=dividendo/divisor
         print("El resultado de la operacion dividir es",resultado)
